08f7b9359d684a2ccebd0f8d82133fcf2a7df045
Raphael Cerqueira <raphaelfc@gmail.com>
2021-07-05 07:55:26 -0300

    Initial Commit

---
+++ DragToClose/DragToCloseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragToCloseApp.swift
+//  DragToClose
+//
+//  Created by Raphael Cerqueira on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct DragToCloseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
